a6e28fb85fb3b8a7806b2c638ff5104186da292d
nitishbytaru <bndnitish24@gmail.com>
2024-01-24 17:48:06 +0530

    make changes in javascript file and made it organized

--- app.js
+++ app.js
@@ -1,66 +1,86 @@
-//life counter
-let counter = 4;
-let startGame = false;
-
+//HTML Element Variables
 let restartButton = document.querySelector(".restart-button");
+let mainBody = document.querySelector(".main-container");
+let life1 = document.getElementById("life1");
+let life2 = document.getElementById("life2");
+let life3 = document.getElementById("life3");
+let resultBoard = document.querySelector(".result");
 
-//button elements
+//User Interactive Buttons
 let rockButton = document.querySelector(".rock-button");
 let paperButton = document.querySelector(".paper-button");
 let scissorsButton = document.querySelector(".scissors-button");
 
-//player elements
-let rockPlayer = document.querySelector(".player-rock");  
+//User Icons
+let rockPlayer = document.querySelector(".player-rock");
 let paperPlayer = document.querySelector(".player-paper");
 let scissorsPlayer = document.querySelector(".player-scissors");
 
-//computer elements
+//Computer Icons
 let rockComputer = document.querySelector(".computer-rock");
 let paperComputer = document.querySelector(".computer-paper");
 let scissorsComputer = document.querySelector(".computer-scissors");
 
-function visible(element) {
-  element.classList.add("visible");
-}
-function invisible(element1, element2) {
-  element1.classList.remove("visible");
-  element2.classList.remove("visible");
-}
-function computerInvisible(element1, element2, element3) {
-  element1.classList.remove("visible");
-  element2.classList.remove("visible");
-  element3.classList.remove("visible");
-}
-
+//Event Listeners
 rockButton.addEventListener("click", (event) => {
   if (startGame === false) {
-    computerInvisible(rockComputer, paperComputer, scissorsComputer);
+    invisible(
+      rockComputer,
+      paperComputer,
+      scissorsComputer,
+      paperPlayer,
+      scissorsPlayer
+    );
     visible(rockPlayer);
-    invisible(paperPlayer, scissorsPlayer);
     computerMove(rockPlayer);
   }
 });
 paperButton.addEventListener("click", (event) => {
   if (startGame === false) {
-    computerInvisible(rockComputer, paperComputer, scissorsComputer);
+    invisible(
+      rockComputer,
+      paperComputer,
+      scissorsComputer,
+      rockPlayer,
+      scissorsPlayer
+    );
     visible(paperPlayer);
-    invisible(rockPlayer, scissorsPlayer);
     computerMove(paperPlayer);
   }
 });
 scissorsButton.addEventListener("click", (event) => {
   if (startGame === false) {
-    computerInvisible(rockComputer, paperComputer, scissorsComputer);
+    invisible(
+      rockComputer,
+      paperComputer,
+      scissorsComputer,
+      rockPlayer,
+      paperPlayer
+    );
     visible(scissorsPlayer);
-    invisible(rockPlayer, paperPlayer);
     computerMove(scissorsPlayer);
   }
 });
+restartButton.addEventListener("click", () => {
+  gameRestart();
+  restartButton.style.display = "none";
+});
 
-// computer logic
+//Utility function to make element visible
+function visible(element) {
+  element.classList.add("visible");
+}
+//Utility function to make element invisible
+function invisible(...elements) {
+  elements.forEach((element) => {
+    element.classList.remove("visible");
+  });
+}
+
+//Computer's move generator
 let computerMoveArr = ["computer-rock", "computer-paper", "computer-scissors"];
 
-//computer's move generator
+//function for computer's move generator
 function computerMove(playerChoice) {
   let computerChoice = document.querySelector(
     `.${computerMoveArr[Math.floor(Math.random() * 3)]}`
@@ -69,7 +89,7 @@ function computerMove(playerChoice) {
   gameResult(playerChoice.id, computerChoice.id);
 }
 
-//checking winner
+//function for checking the game result
 function gameResult(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
     draw(mainBody);
@@ -94,12 +114,7 @@ function gameResult(playerChoice, computerChoice) {
   }
 }
 
-//winning or loseing effects
-let mainBody = document.querySelector(".main-container");
-
-// player winnig
-let resultBoard = document.querySelector(".result");
-
+//Function for player winning
 function playerWon(mainBody) {
   resultBoard.innerText = "Player Won";
   mainBody.classList.add("won");
@@ -108,20 +123,7 @@ function playerWon(mainBody) {
   }, 500);
 }
 
-function gameRestart() {
-  resultBoard.innerText = "Start your game";
-  startGame = false;
-  life1.classList.remove("grey");
-  life2.classList.remove("grey");
-  life3.classList.remove("grey");
-  counter = 4;
-}
-
-restartButton.addEventListener("click", () => {
-  gameRestart();
-  restartButton.style.display = "none";
-});
-
+//Function for computer winning
 function computerWon(mainBody) {
   counter--;
   resultBoard.innerText = "Computer Won";
@@ -140,6 +142,8 @@ function computerWon(mainBody) {
     mainBody.classList.remove("lose");
   }, 500);
 }
+
+//Function if no-one win's
 function draw(mainBody) {
   resultBoard.innerText = "Draw";
   mainBody.classList.add("draw");
@@ -148,11 +152,29 @@ function draw(mainBody) {
   }, 500);
 }
 
-//lifes
-let life1 = document.getElementById("life1");
-let life2 = document.getElementById("life2");
-let life3 = document.getElementById("life3");
+//life counter
+let counter = 4;
+let startGame = false;
 
+//function for losing chances
 function grey(event) {
   event.classList.add("grey");
+}
+
+//GAME RESTART
+function gameRestart() {
+  resultBoard.innerText = "Start your game";
+  startGame = false;
+  life1.classList.remove("grey");
+  life2.classList.remove("grey");
+  life3.classList.remove("grey");
+  counter = 4;
+  invisible(
+    rockComputer,
+    paperComputer,
+    scissorsComputer,
+    paperPlayer,
+    scissorsPlayer,
+    rockPlayer
+  );
 }
