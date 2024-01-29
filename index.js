305618c7f2c980d742064a3c881acd59df07a780
nitishbytaru <bndnitish24@gmail.com>
2024-01-29 23:08:03 +0530

    add login page and add verification and authentication

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/js")));
@@ -11,6 +12,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Parse JSON requests
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "mysecretcode", // Change this to a strong, random string
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -38,44 +46,78 @@ const userSchema = new mongoose.Schema({
 
 const userdetail = mongoose.model("userdetail", userSchema);
 
-app.get("/check", async (req, res) => {
-  let gameUser = await userdetail.find();
-  console.log(gameUser);
-  res.send("hurry");
-});
-
 //node.js application
 app.get("/home", (req, res) => {
   res.render("home.ejs");
 });
 
-app.post("/home", (req, res) => {
+app.post("/home/register", async (req, res) => {
   let userdetail1 = new userdetail({
     username: `${req.body.username}`,
     password: `${req.body.password}`,
   });
-  userdetail1
-    .save()
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  res.render("home.ejs");
+  let gameUser = await userdetail.find({ username: `${req.body.username}` });
+  if (gameUser.length) {
+    //this logic say that there are no such users
+    console.log("erroe user exist");
+    //here a flash message will come
+  } else {
+    userdetail1
+      .save()
+      .then()
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log("new user saved");
+    //here a flash message will come
+    res.redirect("/home");
+  }
+});
+
+app.post("/home/login", async (req, res) => {
+  let gameLoginUser = await userdetail.find({
+    username: `${req.body.username}`,
+  });
+  if (gameLoginUser.length && gameLoginUser[0].password === req.body.password) {
+    req.session.user = gameLoginUser;
+    console.log("user verified");
+    res.redirect("/game");
+    //here a flash message will come
+  } else {
+    //this logic say that there are no such users
+    console.log("user does not exist! Plese register");
+    res.redirect("/home/register");
+    //here a flash message will come
+  }
 });
 
 app.get("/game", (req, res) => {
   res.render("game.ejs");
 });
 
-app.post("/game", (req, res) => {
-  let { highscore } = req.body;
-  console.log(highscore);
+app.post("/game", async (req, res) => {
+  try {
+    let { finalHighscore } = req.body;
+    let presentUserDetails = await userdetail.find({
+      username: `${req.session.user[0].username}`,
+    });
+    let { updated } = await userdetail.updateOne(
+      { username: `${req.session.user[0].username}` },
+      { $cond: { if: { highscore: 0 }, then: finalHighscore, else: 0 } }
+    );
+    console.log("11" + updated);
+    console.log("22" + presentUserDetails);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/home/register", (req, res) => {
-  res.render("register.ejs");
+  res.render("register.ejs", { msg: 0 });
+});
+
+app.get("/home/login", (req, res) => {
+  res.render("login.ejs", { msg: 0 });
 });
 
 app.listen(8080, () => {
